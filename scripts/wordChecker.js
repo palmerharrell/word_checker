625d50a449276e7a6a386980ab95d409fa8b17e7
Palmer Harrell <palmerharrell@me.com>
2016-02-23 09:55:47 -0600

    Added HTML elements and defined three required functions in js

--- scripts/wordChecker.js
+++ scripts/wordChecker.js
@@ -1,6 +1,5 @@
 // Now that the test suite exists, write a simple application to implement the functionality.
 
-// In an HTML file, have two elements: a textarea and a button.
 // When the button is clicked, extract the contents of the textarea and validate it against 
 // the following rules.
 
@@ -9,4 +8,18 @@
 
 // No duplicate words allowed.
 // Word count should be less than 100.
+
+"use strict";
+
+function checkWordCount(testStr) {
+	console.log("checkWordCount");
+}
+
+function duplicateCheck(testStr) {
+	console.log("duplicateCheck");
+}
+
+function verifyAlphaNumeric(testStr) {
+	console.log("verifyAlphaNumeric");
+}
 
