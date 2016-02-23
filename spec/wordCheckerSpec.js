44625dd6e5b4eba2898122dd58a0976de00492f7
Palmer Harrell <palmerharrell@me.com>
2016-02-23 09:42:29 -0600

    Finished writing unit tests

--- spec/wordCheckerSpec.js
+++ spec/wordCheckerSpec.js
@@ -1,35 +1,29 @@
 // Write a test suite to validate your JavaScript functions.
 
-describe("The specification for Word Checker", function() {
+describe("Word Checker", function() {
   
-  // There should be a function defined with the name checkWordCount.
   it("should have a checkWordCount function", function() {
     expect(checkWordCount).toBeDefined();
   });
 
-  // That function should return false if a string sent to it has more than 100 words in it.
   it("checkWordCount should return false if string sent to it is over 100 words", function() {
-    // expect(add(2, 5)).toBe(7);
+    expect(checkWordCount("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis voluptatum, provident mollitia nisi inventore! Impedit itaque perferendis cum aliquam, eius? Quidem esse tenetur amet iste nam quasi sed rem, culpa est earum, error asperiores assumenda debitis, molestiae repellendus autem unde necessitatibus delectus ut vitae maxime nihil accusamus! Quibusdam ad eligendi soluta laboriosam aliquid fuga repellendus porro ipsam sunt, quis temporibus. Quos nostrum tenetur nisi quia adipisci delectus temporibus, praesentium, natus quo est ipsa animi veniam culpa totam non consequatur in ipsum corrupti unde enim dolor! Aperiam error, et a. Commodi officiis odio, consequatur soluta itaque cumque tenetur deleniti eius minima fugit ut blanditiis, amet quidem nesciunt nihil vero assumenda eum.")).toBe(false);
   });
 
-  // There should be a function defined with the name duplicateCheck.
   it("should have a duplicateCheck function", function() {
     expect(duplicateCheck).toBeDefined();
   });
 
-  // That function should return false if a string has duplicate words in it.
   it("duplicateCheck should return false if string has duplicate words in it", function() {
-    // expect(add(2, 5)).toBe(7);
+    expect(duplicateCheck("testing duplicates, testing")).toBe(false);
   });
 
-  // There should be a function defined with the name verifyAlphaNumeric.
   it("should have a verifyAlphaNumeric function", function() {
     expect(verifyAlphaNumeric).toBeDefined();
   });
 
-  // That function should return false if there are any non-alphanumeric characters in a string.
   it("verifyAlphaNumeric should return false if string has non-alphanumeric chars in it", function() {
-    // expect(add(2, 5)).toBe(7);
+    expect(verifyAlphaNumeric("One of these 1s is not a word")).toBe(false);
   });
 
 });
